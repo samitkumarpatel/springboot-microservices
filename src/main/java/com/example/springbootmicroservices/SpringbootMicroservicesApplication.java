697c8216d766babb98e43d2eb457c2b2d08e25fc
samitkumarpatel <samitkumarpatel@gmail.com>
2022-06-30 00:36:47 +0200

    some more example added

--- src/main/java/com/example/springbootmicroservices/SpringbootMicroservicesApplication.java
+++ src/main/java/com/example/springbootmicroservices/SpringbootMicroservicesApplication.java
@@ -1,21 +1,21 @@
 package com.example.springbootmicroservices;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
-
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -32,38 +32,22 @@ public class SpringbootMicroservicesApplication {
 @Component
 class Router {
 	@Bean
-	public RouterFunction<ServerResponse> route() {
-		return RouterFunctions.route(
-				GET("/hello"), Handler::hello);
-	}
-}
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-class Hello {
-	private int id;
-	private String message;
-}
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class HelloException extends RuntimeException {
-	public HelloException() {
-		super();
-	}
-	public HelloException(String message) {
-		super(message);
+	public RouterFunction<ServerResponse> route(Handler handler) {
+		return RouterFunctions
+				.route(GET("/hello"), handler::hello)
+				.andRoute(GET("/notification"), handler::sendNotification);
 	}
 }
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 class Handler {
 	private static final String SUCCESS = "success";
 	private static final String FAILURE = "failure";
-	public static Mono<ServerResponse> hello(ServerRequest serverRequest) {
-		var status = serverRequest.queryParam("status").orElse(SUCCESS);
+	private final Notification notification;
+	public Mono<ServerResponse> hello(ServerRequest request) {
+		var status = request.queryParam("status").orElse(SUCCESS);
 		if (SUCCESS.equalsIgnoreCase(status)) {
 			return ok()
 					.body(
@@ -77,5 +61,73 @@ class Handler {
 					.body(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,"Hello Error")),Hello.class);
 		}
 	}
+
+	public Mono<ServerResponse> sendNotification(ServerRequest request) {
+		var to = "Samit";
+		return ok().body(
+					notification.send(to)
+							.map(m -> m)
+							.doOnNext(m -> log.info("Message sent Successfully to {}",to))
+							.doOnError(e -> Mono.error(e))
+				, Message.class);
+	}
+
 }
+
+interface Notification {
+	Mono<Message> send(String to);
+}
+
+@Service
+@ConditionalOnProperty(prefix = "notification", name = "service", havingValue = "email")
+class EmailNotification implements Notification {
+	@Override
+	public Mono<Message> send(String to) {
+		return Mono.just(
+				Message.builder().msgId(0).message("EMAIL message").source(NotificationType.EMAIL).build()
+		);
+	}
+}
+
+@Service
+@ConditionalOnProperty(prefix = "notification", name = "service", havingValue = "sms")
+class SmsNotification implements Notification {
+	@Override
+	public Mono<Message> send(String to) {
+		return Mono.just(
+				Message.builder().msgId(1).message("SMS message").source(NotificationType.SMS).build()
+		);
+	}
+}
+
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+class Hello {
+	private int id;
+	private String message;
+}
+
+enum NotificationType {
+	EMAIL,
+	SMS
+}
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+class Message {
+	private int msgId;
+	private String message;
+	private NotificationType source;
+}
+
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+class HelloException extends RuntimeException {
+	public HelloException() {
+		super();
+	}
+	public HelloException(String message) {
+		super(message);
+	}
+}
+
+
 
--- src/test/java/com/example/springbootmicroservices/SpringbootMicroservicesApplicationTests.java
+++ src/test/java/com/example/springbootmicroservices/SpringbootMicroservicesApplicationTests.java
@@ -1,7 +1,28 @@
 package com.example.springbootmicroservices;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.RouteMatcher;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.test.StepVerifier;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class SpringbootMicroservicesApplicationTests {
@@ -10,4 +31,54 @@ class SpringbootMicroservicesApplicationTests {
 	void contextLoads() {
 	}
 
+}
+
+@ExtendWith(SpringExtension.class)
+class HandlerTest {
+	@MockBean
+	ServerRequest request;
+	Handler handler;
+	@Test
+	@DisplayName("Check Test Integration Test")
+	void test(){
+		assertEquals(1,1);
+	}
+
+	@Test
+	@DisplayName("EMAIL Notification Test")
+	void emailNotificationTest() {
+		handler = new Handler(new EmailNotification());
+		when(request.queryParam("to")).thenReturn(Optional.of("samit"));
+
+		StepVerifier.create(handler.sendNotification(request)).consumeNextWith(msg -> {
+			assertEquals(HttpStatus.OK, msg.statusCode());
+		}).verifyComplete();
+	}
+
+	@Test
+	@DisplayName("SMS Notification Test")
+	void smsNotificationTest() {
+		handler = new Handler(new SmsNotification());
+		when(request.queryParam("to")).thenReturn(Optional.of("samit"));
+
+		StepVerifier.create(handler.sendNotification(request)).consumeNextWith(msg -> {
+			assertEquals(HttpStatus.OK, msg.statusCode());
+		}).verifyComplete();
+	}
+}
+
+//@SpringBootTest(properties = {"notification.service=email"})
+
+//@SpringBootConfiguration
+//@TestPropertySource(properties = "notification.service=email")
+//@TestPropertySource(properties = {"notification.service=email", "name=ABC"})
+class HandlerIntegrationTest {
+	// 1.) To set the @value field you can use Springs ReflectionTestUtils - it has a method setField to set private fields
+
+	/* 2.)
+	@BeforeClass
+	public static void beforeClass() {
+		System.setProperty("some.property", "<value>");
+	}
+	 */
 }
